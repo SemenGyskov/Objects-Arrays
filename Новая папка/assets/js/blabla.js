bc26080af02d272fb4da8b572da91377cece5068
SemenGyskov <114874984+SemenGyskov@users.noreply.github.com>
2022-11-01 03:37:36 +0500

    Add files via upload

---
+++ Новая папка/assets/js/blabla.js
@@ -0,0 +1,76 @@
+// 1
+// let arr = ['a','b','c']
+// alert(arr)
+
+
+// 2
+// let arr = ['a','b','c']
+// alert(arr[0])
+// alert(arr[1])
+// alert(arr[2])
+
+// 3
+
+// var arr = ['a', 'b', 'c', 'd'] 
+// alert(`${arr[0]}+${arr[1]}`)
+// alert(`${arr[2]}+${arr[3]}`)
+
+// 4
+
+// var arr = [2, 5, 3, 9] 
+// var result = arr[0] * arr[1] + arr[2] * arr[3] 
+// alert(result)
+
+// 5
+
+// var obj = {a: 1, b: 2, c: 3};
+
+// alert(obj.c)
+// alert(obj['c'])
+
+// 6
+
+// var obj = {Коля: '1000', Вася: '500', Петя: '200'};
+// alert(obj.Коля)
+// alert(obj.Петя)
+
+// 7
+
+// var obj = {one:'понедельник', two: 'вторник', three: 'среда', four: 'четверг', five: 'пятница', six: 'суббота', seven:"воскресенье" };
+// alert(obj.five)
+
+// 8
+
+// var obj = {1:'понедельник', 2: 'вторник', 3: 'среда', 4: 'четверг', 5: 'пятница', 6: 'суббота', 7:"воскресенье" };
+// var day = prompt('Введи чило')
+// alert(obj[String(day)])
+
+// 9
+
+// var mas = [ [1, 2, 3], [4, 5, 6], [7,8,9] ];
+// alert(mas[1][0])
+
+// 10
+
+// var obj = {js:['jQuery', 'Angular'], php: 'hello', css: 'world'};
+// alert(obj.js[0])
+
+
+// 11
+
+// var ru = {0: "Понедельник", 1: "Вторник", 2: "Среда", 3: "Четверг", 4: "Пятница", 5: "Суббота", 6: "Воскресенье"}
+// var en = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
+// alert(ru[0])
+// alert(en[2])
+
+// 12
+// var ru = {0: "Понедельник", 1: "Вторник", 2: "Среда", 3: "Четверг", 4: "Пятница", 5: "Суббота", 6: "Воскресенье"}
+// var en = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
+// var lang = prompt('Введите свой язык')
+// var day = prompt('Введите свой день недели')
+// if (lang = ru){
+//     alert(ru[day-1])
+// }
+// else if (lang = en){
+//     alert(en[day-1])
+// }
